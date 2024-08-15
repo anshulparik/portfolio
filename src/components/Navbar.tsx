277c8d56853c9ddc0f6d38067a234ff6dda55f5b
anshulparik <anshulparik@gmail.com>
2024-08-16 04:45:44 +0530

    Navbar responsiveness fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,15 +8,15 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <nav className="mt-4 lg:mt-6 flex items-center justify-between">
-      <div className="ml-1 lg:ml-6">
+      <div className="ml-1 lg:ml-6 relative w-[100px] h-[45px] lg:w-[140px] lg:h-[55px] ">
         <Image
           src="/images/anshul25.png"
           alt="ANSHUL"
-          width={140}
-          height={120}
+          quality={100}
+          fill
         />
       </div>
-      <div className="flex mr-2 lg:mr-8 text-3xl">
+      <div className="flex mr-2 lg:mr-8 text-2xl lg:text-3xl">
         <FaLinkedin className="mr-3 cursor-pointer hover:text-sky-600" />
         <FaGithub className="cursor-pointer hover:text-purple-500" />
       </div>
