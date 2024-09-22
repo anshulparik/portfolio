18f01a7b22555cc72c0daf1618ee755f90fc339e
Anshul Parik <anshulparik@gmail.com>
2024-09-23 01:21:52 +0530

    home bg image added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import NavbarInteraction from "./NavbarInteraction";
 
 const Navbar = () => {
   return (
-    <nav id="nav" className="mt-4 lg:mt-6 flex items-center justify-between">
+    <nav id="nav" className="py-4 lg:py-6 flex items-center justify-between">
       <div
         className="cursor-pointer
         ml-1 lg:ml-6 relative w-[100px] h-[45px] lg:w-[140px] lg:h-[55px]"
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -6,13 +6,18 @@ const Main = () => {
   return (
     <section
       id="main"
-      className="h-[calc(100vh_-_51.76px_-_1rem)] lg:h-[calc(100vh_-_51.76px_-_1.5rem)] 
-      flex flex-col justify-center items-center"
+      className="relative h-[calc(100vh-77px)] lg:h-[calc(100vh-103px)] 
+      flex flex-col justify-center items-center bg-cover bg-center"
+      style={{ backgroundImage: 'url("/images/bg.jpg")' }}
     >
-      <div>
+      <div className="absolute inset-0 bg-black opacity-80" />
+      <div className="relative z-10">
+        <span className="px-2 text-xl md:text-2xl lg:text-3xl font-medium flex gap-2 md:gap-3 lg:gap-4">
+          <span className="text-purple-800">Hello!</span> <span>I am</span>
+        </span>
         <h1 className="px-2 text-5xl md:text-6xl lg:text-9xl font-bold tracking-tight">
           <TextReveal text="Anshul" />{" "}
-          <span className="text-purple-800">
+          <span className="text-white">
             <TextReveal text="Parik" />
           </span>
         </h1>
