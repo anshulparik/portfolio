5a9ea1a4ad9fd875976850e9d54148b824a4822c
Anshul Parik <anshulparik@gmail.com>
2024-09-23 21:04:05 +0530

    bg changes

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -8,9 +8,9 @@ const Main = () => {
       id="main"
       className="relative h-[calc(100vh-77px)] lg:h-[calc(100vh-103px)] 
       flex flex-col justify-center items-center bg-cover bg-center mb-14 lg:mb-40"
-      style={{ backgroundImage: 'url("/images/bg.jpg")' }}
+      style={{ backgroundImage: 'url("/images/bg2.jpg")' }}
     >
-      <div className="absolute inset-0 bg-black opacity-80" />
+      <div className="absolute inset-0 bg-black opacity-[0.87]" />
       <div className="relative z-10">
         <span className="px-2 text-xl md:text-2xl lg:text-3xl font-medium flex gap-2 md:gap-3 lg:gap-4">
           <span className="text-purple-800">Hello!</span> <span>I am</span>
