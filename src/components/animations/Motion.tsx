d771f376b97f86ceaafe49d77746f16144f828cc
Anshul Parik <anshulparik@gmail.com>
2024-09-25 00:58:42 +0530

    animation added to main file text

--- src/components/animations/Motion.tsx
+++ src/components/animations/Motion.tsx
@@ -5,3 +5,4 @@ import { motion } from "framer-motion";
 export const MotionDiv = motion.div;
 export const MotionPara = motion.p;
 export const MotionH3 = motion.h3;
+export const MotionSpan = motion.span;
